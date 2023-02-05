ab248af6ff26db111db70d4535c114d63957be0b
Acidus <acidus99@users.noreply.github.com>
2023-02-04 20:18:20 -0500

    expose best title

--- Models/HtmlMetaData.cs
+++ Models/HtmlMetaData.cs
@@ -6,12 +6,17 @@ namespace HtmlToGmi.Models
         public Uri FeedUrl { get; set; }
         public string MetaTitle { get; set; }
 
-
         public string OpenGraphDescription { get; set; }
         public Uri OpenGraphImage { get; set; }
         public string OpenGraphSiteName { get; set; }
         public string OpenGraphTitle { get; set; }
         public string OpenGraphType { get; set; }
+
+        public bool HasTitle
+            => !string.IsNullOrEmpty(Title);
+
+        public string Title
+            => !string.IsNullOrEmpty(OpenGraphTitle) ? OpenGraphTitle : MetaTitle;
 
     }
 }
